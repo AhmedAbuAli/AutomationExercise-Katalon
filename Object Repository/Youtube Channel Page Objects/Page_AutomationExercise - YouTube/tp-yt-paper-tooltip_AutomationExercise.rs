dfd269a633568247a7a5801438626c833eed3628
AhmedAbuAli <ahmedmarwan105@gmail.com>
2023-02-13 12:13:33 +0300

    A

---
+++ Object Repository/Youtube Channel Page Objects/Page_AutomationExercise - YouTube/tp-yt-paper-tooltip_AutomationExercise.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tp-yt-paper-tooltip_AutomationExercise</name>
+   <tag></tag>
+   <elementGuidId>88e8e9f9-69aa-4fea-af57-b2564fe9fdc9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='container']/tp-yt-paper-tooltip</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>tp-yt-paper-tooltip.style-scope.ytd-channel-name</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>tp-yt-paper-tooltip</value>
+      <webElementGuid>812350de-7262-4f01-b195-0cae9ee99ac7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>style-scope ytd-channel-name</value>
+      <webElementGuid>472e8e52-ae50-4f36-a3eb-f0f57f267060</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>tooltip</value>
+      <webElementGuid>d268a353-751b-4f95-8586-02a1f12861f6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>-1</value>
+      <webElementGuid>fcabf985-7424-41e9-acbb-fac0d51d2f02</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+  
+    AutomationExercise
+  
+
+</value>
+      <webElementGuid>38cf146a-9643-4d30-81bd-83e7c5196e1e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;inner-header-container&quot;)/div[@id=&quot;meta&quot;]/ytd-channel-name[@id=&quot;channel-name&quot;]/div[@id=&quot;container&quot;]/tp-yt-paper-tooltip[@class=&quot;style-scope ytd-channel-name&quot;]</value>
+      <webElementGuid>ce3a5e5a-7d11-4d74-9c5a-9886530a75af</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='container']/tp-yt-paper-tooltip</value>
+      <webElementGuid>326fd9f3-9bfa-40d6-a4b9-4cb550ba7186</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='AutomationExercise'])[1]/following::tp-yt-paper-tooltip[1]</value>
+      <webElementGuid>3544c2b4-1a7e-4d8a-bb6f-b5484cca68a5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='@AutomationExercise'])[1]/preceding::tp-yt-paper-tooltip[1]</value>
+      <webElementGuid>49975bdc-84c7-4ea0-b3f0-2a43a0b7e7d1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ytd-channel-name/div/tp-yt-paper-tooltip</value>
+      <webElementGuid>3a07ab63-0078-48ee-acd1-e7b977e8cb7b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//tp-yt-paper-tooltip[(text() = '
+  
+    AutomationExercise
+  
+
+' or . = '
+  
+    AutomationExercise
+  
+
+')]</value>
+      <webElementGuid>8d76cd4b-6aa7-4109-aacf-7c794ffcaa7e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
